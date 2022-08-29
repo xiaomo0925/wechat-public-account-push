f71aa74616d9493e95d6f3fbb0b7e581b62a1235
xiaomo0925 <112382100+xiaomo0925@users.noreply.github.com>
2022-08-29 23:06:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wx3aa86a1e4af3321f",
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "b0d0e3efc8d2ad013e6c32c950a4c299",
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: "5u-idupsUJ_vhT62IeYis4V6PC4zh9dKHAVMQp06ido",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: [""],
+    USERS: ["ol_M16H5B1jj5qzroObnnsS3jTY8"],
      
     /**
      * 信息配置
@@ -19,9 +19,9 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "四川",
     // 所在城市
-    CITY: "惠州",
+    CITY: "九寨沟",
 
     /** 重要节日相关 */
 
@@ -30,7 +30,7 @@ export const config = {
     // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
     // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
     FESTIVALS: [
-      {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
+      {"type": "生日", "name": "老婆", "year": "1998", "date": "09-25"},
       {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
       {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
       {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
